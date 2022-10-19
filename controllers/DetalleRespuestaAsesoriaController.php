ca293c7c39837fe904a869e66134cf7076d321e5
FelixHerrera2022 <111821308+FelixHerrera2022@users.noreply.github.com>
2022-10-19 20:29:11 -0300

    Correccion DetalleRespuestaAsesoriaContoller

--- controllers/DetalleRespuestaAsesoriaController.php
+++ controllers/DetalleRespuestaAsesoriaController.php
@@ -3,24 +3,23 @@
 class DetalleRespuestaAsesoriaController{
     function __construct(){
         //echo "esto funciona";
-       $id_respuesta_asesoria = "";
+        $id_respuesta_asesoria = "";
+        
+       if(isset($_GET['id_respuesta_asesoria']) || !is_null($_GET['id_respuesta_asesoria'])){
 
+        $id_respuesta_asesoria = $_GET['id_respuesta_asesoria'];
+
+        }
            //Lo primero es llamar el modelo
 
         require_once "models/RespuestaAsesoriaModel.php";
         $model_respuesta_asesoria = new RespuestaAsesoriaModel();
-        //$model_respuesta = new RespuestaAsesoriaModel();
-
 
         //Llamar datos del modelo
         
-    
-        //$datos_respuesta_asesoria = $model_respuesta_asesoria->getById($id_respuesta_asesoria);
-        //$datosusuariocliente = $_SESSION['usuarioCliente'];
-          $datosusuariocliente = $_SESSION['usuarioCliente'];
-          $datos_respuesta_asesoria = $model_respuesta_asesoria->getById($id_respuesta_asesoria);
-          //$datos_respuesta =  $model_respuesta->getAllByCliente($datosusuariocliente['id_cliente']);
 
+        $datos_respuesta_asesoria = $model_respuesta_asesoria->getById($id_respuesta_asesoria);
+        
 
         //Llamar a la vista 
         require_once "views/layout/header.php";
